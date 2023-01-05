a9e0d58241cf673ee53368a721aa726066e12ff8
Sartajkhan01 <sartajikbalkhan2000@gmail.com>
2023-01-05 14:15:27 +0530

    [Sartaj] : Added Flip Coin Percentage

---
+++ BasicCorePrograms/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Basic Programming Problems");
+            bool end = true;
+            Console.WriteLine("1. FlipCoin\n2. End the Program ");
+            while (end)
+            {
+                Console.WriteLine("Choose an option to execute");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flipcoin = new FlipCoin();
+                        flipcoin.Flip();
+                        break;
+                    case 2:
+                        end = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choose the correct option");
+                        break;
+                }
+            }
+
+        }
+
+    }
+}
